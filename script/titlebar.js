34d9b7bb00adf6dfcc7f2389ec0e1858bc06660c
MeguMario <ttrungpt2@gmail.com>
2024-03-26 17:44:55 +0700

    reworked the taskbar for mobile

--- script/titlebar.js
+++ script/titlebar.js
@@ -3,10 +3,10 @@ webelement_path= "/elements/"
 var t_object = ["section","section"]
 var sec0_attr = ["home","subpages"]
 
-var subpages = ["button","button","button","button"]
-var subpages_name = ["Donate","Links","Manga","Winnyace"]
-var subpages_icon = ["favorite","person","menu_book","question_mark"]
-var subpages_dest = ["'/donate.html'","'/info.html'","'/manga/main.html'","'/winnyace.html'"]
+var subpages = ["button","button","button","button","button"]
+var subpages_name = ["Home","Donate","Links","Manga","Winnyace"]
+var subpages_icon = ["home","favorite","person","menu_book","question_mark"]
+var subpages_dest = ["'/index.html'","'/donate.html'","'/info.html'","'/manga/main.html'","'/winnyace.html'"]
 
 titlebar(); //init titlebar
 
@@ -51,11 +51,10 @@ function titlebar(){
    })
    document.getElementById('homebotan_link').append(homebotan)
 
-   // home button for mobilebar
-   var homebotan_func = document.createElement("a")
+   // home button for mobilebar disable
+   /*var homebotan_func = document.createElement("a")
    Object.assign(homebotan_func, {
-        id: 'homebotan_link',
-        href: '/index.html'
+        id: 'homebotan_link'
    })
    document.getElementById("home_mobile").append(homebotan_func)
    var homebotan = document.createElement("img")
@@ -64,10 +63,11 @@ function titlebar(){
         id: 'asuka_homebutton',
    })
    document.getElementById('homebotan_link').append(homebotan)
+   */
 
 
    // subpages spawn 
-   for (var i = 0; i < subpages.length ; i++){
+   for (var i = 1; i < subpages.length ; i++){
         var sp_obj =  document.createElement(subpages[i])
         Object.assign(sp_obj, {
             innerHTML: subpages_name[i],
@@ -85,18 +85,28 @@ function titlebar(){
     for (var i = 0; i < subpages.length ; i++){
         var sp_obj =  document.createElement(subpages[i])
         Object.assign(sp_obj, {
-            innerHTML: subpages_name[i],
             id: subpages_name[i] + '_mobilebar_button',
             type: 'button'
         })
         document.getElementById("subpages_mobile").append(sp_obj)
-
+        
+        /*
+        var sp_obj_text = document.createElement('p')
+        Object.assign(sp_obj_text, {
+            id: 'sp_obj_text',
+            innerHTML: subpages_name[i]
+        })
+        document.getElementById(subpages_name[i] + '_mobilebar_button').append(sp_obj_text)
+        */
+       
         var sp_obj_img = document.createElement('span')
         Object.assign(sp_obj_img, {
             id: subpages_name[i] + '_mobile',
             innerHTML: subpages_icon[i]
         })
-        sp_obj_img.setAttribute('class','material-icons-round')
+
+        sp_obj_img.setAttribute('class','material-icons-round sp_obj_mobile_icon')
+        sp_obj.setAttribute('class','sp_obj_button')
         document.getElementById(subpages_name[i] + '_mobilebar_button').append(sp_obj_img)
 
         sp_obj.setAttribute('onclick', 'location.href=' + subpages_dest[i])
