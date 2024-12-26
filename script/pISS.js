bfa1d8800c67b6f0c199514e517c41a70e023f61
MeguMario <ttrungpt2@gmail.com>
2024-12-27 01:27:58 +0700

    fixing some bug related to the display of the emoji

--- script/pISS.js
+++ script/pISS.js
@@ -14,34 +14,27 @@ const div_titlebar = document.getElementById("home")
 div_titlebar.append(piss_counter)
 piss_counter.append(piss_status)
 
-function statusIcon_create(statusValue){
-    piss_status.insertAdjacentHTML("beforebegin","🧑‍🚀🚽")
+piss_status.insertAdjacentHTML("beforebegin","<p id=piss_chip_sym>🧑‍🚀🚽</p>")
 
+function statusIcon_create(statusValue){
     if (statusValue == undefined){
         piss_status.innerHTML = "..."
-    }
-    if (statusValue == undefined){
         piss_counter.style.setProperty("--bg-progress", "rgba(150,150,150,0.75)", "important")
         piss_counter.style.setProperty("--bg-width", `0%`, "important")
     }
     else if (statusValue < 50){
         piss_counter.style.setProperty("--bg-progress", "rgba(50,205,50,0.75)", "important")
-        piss_counter.style.setProperty("--bg-width", `${statusValue}%`, "important")
-       
+
     }
     else if (statusValue >= 50, statusValue < 75){
         piss_counter.style.setProperty("--bg-progress", "rgba(255,240,0,0.75)", "important")
-        piss_counter.style.setProperty("--bg-width", `${statusValue}%`, "important")
     }
     else if (statusValue >= 75){
         piss_counter.style.setProperty("--bg-progress", "rgba(255,76,48,0.75)", "important")
         piss_counter.style.setProperty("color", "black", "important")
-        piss_counter.style.setProperty("--bg-width", `${statusValue}%`, "important")
     }
+    piss_counter.style.setProperty("--bg-width", `${statusValue}%`, "important")
 }
-
-
-
 
 require(["LightstreamerClient", "Subscription", "StaticGrid"], 
     function dataFetch(LightstreamerClient, Subscription, StaticGrid) {
@@ -53,7 +46,6 @@ require(["LightstreamerClient", "Subscription", "StaticGrid"],
         var progressbar = function progress(){
             let statusValue = grid.values.matrix[`${ls_item}`][`${ls_field}`]
             statusIcon_create(statusValue)
-            
         }
 
         var subscription = new Subscription("MERGE",ls_item,ls_field);
@@ -62,7 +54,6 @@ require(["LightstreamerClient", "Subscription", "StaticGrid"],
         subscription.addListener({onItemUpdate: progressbar});
 
         client.subscribe(subscription);
-        
     })
 
 
